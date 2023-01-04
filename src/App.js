4a016e57b5ecbaf31753282031377d27b602de8a
tyler <tylermontobbio@gmail.com>
2023-01-04 14:06:26 -0800

    test buttons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import dogIM from './img/dogim.jpg'
+import movieHunter from './img/movie-hunter.png'
 
 function App() {
   return (
   <div className="App">
-    <div id='main-container'>asdasdasd</div>
+    <div id='main-container'>
+      <div className='project-tile'>
+        <a href="https://moviehunter.tmontobbio.dev/">
+          <img src={movieHunter} alt="movie-hunter" />
+        </a>
+      </div>
+      <div className='project-tile'>
+        <a href="https://dogim.tmontobbio.dev/">
+          <img src={dogIM} alt="dog-im" />
+        </a>
+      </div>
+    </div>
   </div>
   )
 }
