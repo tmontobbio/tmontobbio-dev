bb2d0ea03617f335468f6a9882d3125374c79db2
tyler <tylermontobbio@gmail.com>
2023-01-06 14:07:56 -0800

    descriptions, formatting, relative font sizing

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -12,9 +12,9 @@ export default function Contact() {
     return (
         <div className="child">
             <ul id="contact-list">
-                <a href="mailto:tylermontobbio@gmail.com" target="_blank"><li className="con-sel"><img src={gmail} alt="gmail-icon" className="contact-icon" />mail</li></a>
-                <a href="https://www.linkedin.com/in/tmontobbio" target="_blank"><li className="con-sel"><img src={linkedin} alt="linkedin-icon" className="contact-icon" />linkedin</li></a>
-                <a href="https://github.com/tmontobbio" target="_blank"><li className="con-sel"><img src={github} alt="github-icon" className="contact-icon" />github</li></a>
+                <a href="mailto:tylermontobbio@gmail.com" target="_blank"><li><img src={gmail} alt="gmail-icon" className="contact-icon" />mail</li></a>
+                <a href="https://www.linkedin.com/in/tmontobbio" target="_blank"><li><img src={linkedin} alt="linkedin-icon" className="contact-icon" />linkedin</li></a>
+                <a href="https://github.com/tmontobbio" target="_blank"><li><img src={github} alt="github-icon" className="contact-icon" />github</li></a>
             </ul>
         </div>
     )
