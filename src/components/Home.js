ba88889082abd10e1c421d167584cf90fd646988
tyler <tylermontobbio@gmail.com>
2023-01-19 15:44:53 -0800

    navbar fixed?

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Home() {
     return (
         <div className="center" id="home">
-            hello.
+            greetings.
         </div>
     )
 }
