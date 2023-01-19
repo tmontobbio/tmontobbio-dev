bc6eedc660b865b691bc2f04c842a57ce6a19b34
tyler <tylermontobbio@gmail.com>
2023-01-19 15:17:57 -0800

    mobile friendly css

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,11 +7,9 @@ export default function Contact() {
 
     return (
         <div className="center">
-            <ul id="contact-list">
-                <div className="contact-tile"><a href="mailto:tylermontobbio@gmail.com" target="_blank" rel="noreferrer"><li><img src={gmail} alt="gmail-icon" className="contact-icon" />mail</li></a></div>
-                <div className="contact-tile"><a href="https://www.linkedin.com/in/tmontobbio" target="_blank" rel="noreferrer"><li><img src={linkedin} alt="linkedin-icon" className="contact-icon" />linkedin</li></a></div>
-                <div className="contact-tile"><a href="https://github.com/tmontobbio" target="_blank" rel="noreferrer"><li><img src={github} alt="github-icon" className="contact-icon" />github</li></a></div>
-            </ul>
+            <div className="contact-tile"><a href="mailto:tylermontobbio@gmail.com" target="_blank" rel="noreferrer"><img src={gmail} alt="gmail-icon" className="contact-icon" /></a></div>
+            <div className="contact-tile"><a href="https://www.linkedin.com/in/tmontobbio" target="_blank" rel="noreferrer"><img src={linkedin} alt="linkedin-icon" className="contact-icon" /></a></div>
+            <div className="contact-tile"><a href="https://github.com/tmontobbio" target="_blank" rel="noreferrer"><img src={github} alt="github-icon" className="contact-icon" /></a></div >
         </div>
     )
-}
+}   
